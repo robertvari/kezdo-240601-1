293a3201c14e98ac82bc785b53dcb6d833e50a4a
Robert Vari <mail.robertvari@gmail.com>
2024-06-01 17:16:35 +0200

    update

---
+++ flip_my_name.py
@@ -0,0 +1,6 @@
+print("Hello there! Let's play a little game.")
+print("Give me your name and I give back its encrypted version.")
+
+name = input("What is your name? ")
+
+print(f"Your encrypted name is: {name[::-1]}")
