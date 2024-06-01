0f891682d4d5608485c3c35646d3eb7106c48ea0
Robert Vari <mail.robertvari@gmail.com>
2024-06-01 15:57:10 +0200

    update

--- 05_lists.py
+++ 05_lists.py
@@ -1,5 +1,7 @@
 #                -4        -3        -2        -1
 #                0         1         2         3
 my_friends = ["Csaba", "Kriszta", "Tamás", "Aladár"]
+my_lucky_numbers = [3, 12, 45, 30, 70]
+my_things_and_stuff = [1, 2.123, "Kriszta", my_friends]
 
-print(my_friends[-2])
+print(my_things_and_stuff[-1][-1])
