0dfee19a0ba0a3790657f1950f55abf037c4a17f
Robert Vari <mail.robertvari@gmail.com>
2024-06-01 16:35:03 +0200

    update

--- 06_append_to_list.py
+++ 06_append_to_list.py
@@ -2,5 +2,12 @@
 my_friends = ["Csaba", "Kriszta", "Tamás", "Aladár"]
 
 my_friends.append("Johnie")
-my_friends.append("Tom")
+
+my_lucky_numbers = [1, 2, 3, 4, 5]
+my_friends.extend(my_lucky_numbers)
+
+# my_friends += my_lucky_numbers
+
+my_friends.insert(0, 100)
+
 print(my_friends)
