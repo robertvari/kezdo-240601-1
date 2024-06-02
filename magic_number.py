ed7a00a2701b041ad161695b10dbbbb448ce3ce8
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 15:47:34 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -9,3 +9,11 @@ try_count = 3
 magic_number = random.randint(min_number, max_number)
 
 # Get a number from player
+player_number = input("What is your guess? ")
+
+# check player's number. If wrong ask again.
+while magic_number != player_number:
+    print("Wrong guess! Try again.")
+    player_number = input("What is your guess? ")
+
+print(f"You win! {magic_number} was my number! :)")
