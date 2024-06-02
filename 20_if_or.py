e4d8added8bebb75050ff935a75eab320a7a8027
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 15:02:14 +0200

    update

--- 20_if_or.py
+++ 20_if_or.py
@@ -5,3 +5,8 @@ if (today == "saturday") or (today == "sunday"):
     print("It is a weekend")
 else:
     print("It is a weekday.")
+
+
+# using not to negate the whole condition
+if not((today == "saturday") or (today == "sunday")):
+    pass
