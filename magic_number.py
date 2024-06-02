c3dff8579292030c54b3292ed0336306d5f213cd
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 15:41:44 +0200

    update

---
+++ magic_number.py
@@ -0,0 +1,11 @@
+import random
+
+# Game rules
+min_number = 1
+max_number = 10
+try_count = 3
+
+# Get random number between min_number-max_number
+magic_number = random.randint(min_number, max_number)
+
+# Get a number from player
