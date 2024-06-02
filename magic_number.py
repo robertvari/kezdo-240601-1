ae1c088e552084c56e86d44ff3821a129f23c2da
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 16:01:10 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -6,10 +6,11 @@ max_number = 10
 try_count = 3
 
 # Get random number between min_number-max_number
-magic_number = random.randint(min_number, max_number)
+magic_number = str(random.randint(min_number, max_number))
 
 # Get a number from player
 player_number = input("What is your guess? ")
+
 
 # check player's number. If wrong ask again.
 while magic_number != player_number:
