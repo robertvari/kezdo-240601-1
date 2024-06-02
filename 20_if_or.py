280eccd03cd90fd2b2a9df4b6e640d887aa92198
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 14:59:50 +0200

    update

---
+++ 20_if_or.py
@@ -0,0 +1,7 @@
+today = "monday"
+
+          # False                 # False         
+if (today == "saturday") or (today == "sunday"):
+    print("It is a weekend")
+else:
+    print("It is a weekday.")
