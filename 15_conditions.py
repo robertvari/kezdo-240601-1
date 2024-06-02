bb6e18f2a93c17c8620de0226e3ace19c76d4208
Robert Vari <mail.robertvari@gmail.com>
2024-06-02 14:22:00 +0200

    update

---
+++ 15_conditions.py
@@ -0,0 +1,24 @@
+number = 10
+
+if number == 10:
+    print("The number is 10")
+
+if number != 10:
+    print("Number is not 10")
+
+if number < 10:
+    print("Number is less than 10")
+
+if number > 10:
+    print("Number is greater than 10")
+
+if number <= 10:
+    print("The number is 10 or less")
+
+if number >= 10:
+    print("The number is 10 or greater")
+
+
+# Close an if statement with pass if empty
+if number == 10:
+    pass
